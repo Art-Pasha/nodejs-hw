d2702cddc4a4bf50cd45ceac07d440bb003b9f8c
Artemenko Pavlo <artemnko67@gmail.com>
2026-09-09 10:33:54 +0300

    Fix error handling in connectMongoDB, notesController and errorHandler

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -3,7 +3,11 @@ import mongoose from 'mongoose';
 export const connectMongoDB = async () => {
   const MONGO_URL = process.env.MONGO_URL;
 
-  await mongoose.connect(MONGO_URL);
-
-  console.log('✅ MongoDB connection established successfully');
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log('✅ MongoDB connection established successfully');
+  } catch (error) {
+    console.error('❌ Failed to connect to MongoDB:', error.message);
+    process.exit(1);
+  }
 };
