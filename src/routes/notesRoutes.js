212d88c010148f36ef1ffcc04af2b3e6dd332338
Artemenko Pavlo <artemnko67@gmail.com>
2026-09-23 11:44:42 +0300

    Implement 04-auth task

---
+++ src/routes/notesRoutes.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { authenticate } from '../middleware/authenticate.js';
+import {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  updateNote,
+  deleteNote,
+} from '../controllers/notesController.js';
+import {
+  getAllNotesSchema,
+  noteIdSchema,
+  createNoteSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
+
+const router = Router();
+
+router.use(authenticate);
+
+router.get('/notes', getAllNotesSchema, getAllNotes);
+router.get('/notes/:noteId', noteIdSchema, getNoteById);
+router.post('/notes', createNoteSchema, createNote);
+router.patch('/notes/:noteId', updateNoteSchema, updateNote);
+router.delete('/notes/:noteId', noteIdSchema, deleteNote);
+
+export default router;
